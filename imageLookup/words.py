88aec236b25b3bc5ea35b81fed40c74ce44db293
Sibosop <brian@eastshore.com>
2016-05-17 17:31:08 -0700

    recovered from textmate samba clusterfuck

---
+++ imageLookup/words.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import adGlobal
+import random
+
+debug=True
+
+class Words:
+  
+  lines = []
+  wordList = None
+  wordDir = None
+
+  def __init__(self):
+    self.wordList = adGlobal.wordList
+    self.wordDir= adGlobal.wordDir
+    for w in self.wordList:
+      f = open(self.wordDir+"/"+w,"r")
+      for l in f.read().split('\n'):
+        self.lines.append(l)
+    
+    #self.lines = open(self.wordFile).read().split('\n')
+
+  def getWords(self):
+    tests=[]
+    for i in range(0,2):
+      n = random.randint(0,len(self.lines)-1)
+      tests.append(self.lines[n])
+    if debug: print "tests:",tests[0],tests[1] #,tests[2]
+    return tests
+
+if __name__ == '__main__':
+  w=Words()
+  choices = w.getWords()
+  if debug: print choices
