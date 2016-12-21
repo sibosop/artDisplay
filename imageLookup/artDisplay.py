de73c8858933b8a8e76fc3c94e996d4688948403
Brian Reinbolt <brian@eastshore.com>
2016-12-21 02:01:52 +0000

    turn off search engines

--- imageLookup/artDisplay.py
+++ imageLookup/artDisplay.py
@@ -60,10 +60,10 @@ if __name__ == '__main__':
     tic = threading.Thread(target=imageChecker.imageChecker)
     tic.setDaemon(True)
     tic.start()
-    schedule.every().day.at("07:34").do(changeSearch,"Google")
-    schedule.every().day.at("08:14").do(changeSearch,"Archive")
-    schedule.every().day.at("20:00").do(changeSearch,"Bing")
-    schedule.every().day.at("20:30").do(changeSearch,"Archive")
+    #schedule.every().day.at("07:34").do(changeSearch,"Google")
+    #schedule.every().day.at("08:14").do(changeSearch,"Archive")
+    #schedule.every().day.at("20:00").do(changeSearch,"Bing")
+    #schedule.every().day.at("20:30").do(changeSearch,"Archive")
     while True:
       schedule.run_pending()
       if hp:
