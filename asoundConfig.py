5e0dafea402270856ecc415c9a07c6a31f999c96
brian reinbolt <brian@eastshore.com>
2017-07-04 11:45:54 -0700

    asoundConfig.py ready for tests

--- asoundConfig.py
+++ asoundConfig.py
@@ -9,23 +9,42 @@ debug=True
 micKey="MIC_CARD"
 speakerKey="SPEAKER_CARD"
 
+usbMic="USB-Audio - USB PnP Sound Device"
+usbSpeaker="USB-Audio - USB2.0 Device"
+
 micNum = "1"
 speakerNum = "2"
+
+def getCardNum(line,key):
+  rval=""
+  if line.find(key) != -1:
+    if debug: syslog.syslog("found "+key+":"+line)
+    rval = line.split()[0].strip()
+  return rval
+  
 
 def main():
   cardPath = "/proc/asound/cards"
   path = adGlobal.progDir+"/asoundrc.template"
-  rcPath = home+".asoundrc"
+  rcPath = home+"/.asoundrc"
   try:
     if debug: syslog.syslog("HOME="+home+" Prog="+adGlobal.progDir)
+    with open(cardPath) as f:
+      for line in f:
+        t = getCardNum(line,usbMic)
+        if t != "":
+          micNum = t
+        t = getCardNum(line,usbSpeaker)
+        if t != "":
+          speakerNum = t
+    rc = open(rcPath,"w")
     with open(path) as f:
       for line in f:
-        line = line.rstrip()
         if line.find(micKey) != -1:
           line = line.replace(micKey,micNum)
         elif line.find(speakerKey) != -1:
           line = line.replace(speakerKey,speakerNum)
-        print line
+        rc.write(line)
   except Exception, e:
     syslog.syslog("player error: "+repr(e))
 
