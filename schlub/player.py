191a54433f40b6441e0312fa4780773f5cd321b2
brian reinbolt <brian@eastshore.com>
2017-07-15 22:49:29 -0700

    integrated slp get hosts cmd

--- schlub/player.py
+++ schlub/player.py
@@ -10,6 +10,7 @@ import sys
 import random
 import urllib
 import soundFile
+import slp
 
 
 debug = True
@@ -44,23 +45,7 @@ class playerThread(threading.Thread):
           time.sleep(2)
           continue
         first = True
-        hosts = []
-        services = subprocess.check_output(["slptool"
-                        ,"findsrvs","service:schlub.x"]).split('\n')
-        if len(services) == 0:
-          syslog.syslog("no available services")
-          continue
-        for s in services:
-          if debug: syslog.syslog("slp s:"+s)
-          loc=s.split(',');
-          if loc[0] == '':
-            continue
-          if debug: syslog.syslog("loc:"+str(loc))
-          #attr=subprocess.check_output(["slptool","findattrs",loc[0]]);
-          host={}
-          host['ip']=loc[0].split("//")[1]
-          if debug: syslog.syslog("slp host"+str(host))
-          hosts.append(host)
+        hosts = slp.getHosts("schlub")
         e = soundFile.getSoundEntry()
         choice = e.name
         if debug: syslog.syslog("player choosing "+choice)
