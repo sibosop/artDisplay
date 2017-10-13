4c33095fbc477a6f998bff7b19fea73bcadd63d0
sibosop <brian@eastshore.com>
2017-10-12 18:00:50 -0700

    added Rescan to sendSounds

--- schlub/sendSounds.py
+++ schlub/sendSounds.py
@@ -2,11 +2,52 @@
 import sys
 import slp
 import subprocess
+import urllib2
+import json
+master=""
+
+def doProbe(h):
+  global master
+  ip = h['ip']
+  print("sending request to ",ip)
+  url = "http://"+ip+":8080"
+  print("url:",url)
+  cmd = { 'cmd' : "Probe" , 'args' : "" }
+  req = urllib2.Request(url
+            ,json.dumps(cmd),{'Content-Type': 'application/json'})
+  f = urllib2.urlopen(req)
+  test = f.read()
+  print("got response:",test)
+  s = json.loads(test)
+  print ("is Master",s['isMaster'])
+  if ( s['isMaster'] ):
+    master = ip
+
+def doRescan():
+  global master
+  if ( master == "" ):
+    print ("NO MASTER NO RESCAN")
+    return
+
+  ip = master
+  print("sending request to ",ip)
+  url = "http://"+ip+":8080"
+  print("url:",url)
+  cmd = { 'cmd' : "Rescan" , 'args' : "" }
+  req = urllib2.Request(url ,json.dumps(cmd),{'Content-Type': 'application/json'})
+  f = urllib2.urlopen(req)
+  test = f.read()
+  print("got response:",test)
+
 
 print "getting hosts"
 hosts = slp.getHosts("schlub")
 for h in hosts:
   print h['ip']
+  doProbe(h)
+
+print ("master:",master)
+exit
 
 first = True
 for a in sys.argv:
@@ -19,6 +60,7 @@ for a in sys.argv:
       print cmd
       output = subprocess.check_output(cmd)
 
+    doRescan()
   except Exception, e:
     print str(e)
 
