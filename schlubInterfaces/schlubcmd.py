8967ac7d35f62dc8c033ba4d201a4ccb927655dd
brian reinbolt <brian@eastshore.com>
2018-02-05 22:22:37 -0800

    updated schlubcmd to POST paradigm

--- schlubInterfaces/schlubcmd.py
+++ schlubInterfaces/schlubcmd.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 import subprocess
-import urllib
+import urllib2
 import os
 home = os.environ['HOME']
 import sys
-import slp
 sys.path.append(home+"/GitProjects/artDisplay/imageLookup")
+sys.path.append(home+"/GitProjects/artDisplay/schlub")
 import adGlobal
+import slp
+import json
 
 debug=True
 
@@ -14,6 +16,7 @@ hosts = []
 
 
 def printHostList():
+  global hosts
   print "Host list:"
   for h in hosts:
     o = h['ip']
@@ -23,130 +26,80 @@ def printHostList():
     print " ",o
   print
 
+def sendToHost(ip,cmd,args):
+  print "send to host:",ip,cmd,args
+  url = "http://"+ip+":8080"
+  print("url:"+url)
+  cmd = { 'cmd' : cmd , 'args' : args }
+  print("cmd json:"+json.dumps(cmd))
+  req = urllib2.Request(url
+              ,json.dumps(cmd),{'Content-Type': 'application/json'})
+  f = urllib2.urlopen(req)
+  test = f.read()
+  print("got response:"+test)
+
+def sendToHosts(cmd,args):
+  for h in hosts:
+    sendToHost(h['ip'],cmd,args)
+
 def printCmds():
   print "SchlubCmds:"
-
-  print " auto - set auto play mode"
-  print " hosts - print host list"
-  print " manual - set manual play mode"
-  print " off - power off hosts"
-  print " phrase (line) - speak the give line"
-  print " play (filename) - play the given wav filename (minus .wav)"
-  print " probe - probe hosts"
-  print " reboot - reboot hosts"
-  print " refresh - refresh sound file table on master"
-  print " reset - reset hosts"
-  print " rescan - rescan sound file table on master"
-  print " stop - stop program"
-  print " threads (count) - set number of player threads"
-  print " q - exit the program"
-  print " upgrade - git upgrade the software and reboot"
-  print " vol (val) - set volume to val"
+  for c in cmds:
+    print c
   print
 
-def sendCmd(ip,cmd):
-  try:
-    request = "http://"+ip+":8080/"+cmd
-    if debug: print "sending cmd:",request
-    f = urllib.urlopen(request)
-    test = f.read()
-    print ip,"got response:",test
-  except Exception, e:
-    print "sndCmd:",repr(e)
+def doCmd(cmd): 
+  sendToHosts(cmd[0],[""])
+  return 0
 
-def sendToHosts(cmd):
-  local = ""
-  for h in hosts:
-    if adGlobal.isLocalHost(h['ip']):
-      local = h['ip']
-    else:
-      print "sending ",cmd,"command to",h['ip']
-      sendCmd(h['ip'],cmd)
-  if local != "":
-    print "sending ",cmd,"command to local",local
-    sendCmd(local,cmd)
+def doNum(cmd):
+  sendToHosts(cmd[0],[cmd[1]])
+  return 0
 
-def vol(val):
-  cmd = "vol?val="+val
-  sendToHosts(cmd)
+def doPhrase(cmd):
+  arg = ""
+  for c in cmd[1:]:
+    arg += c + " "
 
-def threads(n):
-  cmd = "threads?n="+n
-  sendToHosts(cmd)
+    
+  sendToHosts(cmd[0],[arg[:-1]])
+  return 0
 
-# request;http://192.168.20.104:8080/player?play=filename
-def play(filename):
-  cmd = "player?play="+filename+".wav"
-  sendToHosts(cmd)
 
-def phrase(line):
-  line.pop(0)
-  cmd = "phrase?p="
-  for l in line:
-    cmd += l + "-"
-  sendToHosts(cmd)
 
-def main():
-  global hosts
+def doQuit(cmd):
+  print cmd
+  return -1
+
+cmds = {
+      'Probe'     : doCmd
+      ,'Volume'   : doNum
+      ,'Phrase'   : doPhrase
+      ,'Threads'  : doNum
+      ,'Poweroff' : doCmd
+      ,'Reboot'   : doCmd
+      ,'Upgrade'  : doCmd
+      ,'PhraseScatter' : doPhrase
+      ,'MaxEvents' : doNum
+      ,'Quit' : doQuit
+    }
+
+
+if __name__ == '__main__':
   run=True
   print "getting host list"
   hosts = slp.getHosts("schlub")
   printHostList()
   printCmds()
   while run:
-   inp=raw_input("schlub-> ")
-   cmd = inp.split(" ");
-   if cmd[0] == "q":
-     run=False
-   elif cmd[0] == "reset":
-     hosts = slp.getHosts("schlub")
-     printHostList()
-   elif cmd[0] == "hosts":
-     printHostList()
-   elif cmd[0] == "off":
-     sendToHosts("poweroff")
-   elif cmd[0] == "reboot":
-     sendToHosts("reboot")
-   elif cmd[0] == "stop":
-     sendToHosts("stop")
-   elif cmd[0] == "probe":
-     sendToHosts("probe")
-   elif cmd[0] == "upgrade":
-     sendToHosts("upgrade")
-   elif cmd[0] == "refresh":
-     sendToHosts("refresh")
-   elif cmd[0] == "rescan":
-     sendToHosts("rescan")
-   elif cmd[0] == "auto":
-     sendToHosts("auto")
-   elif cmd[0] == "manual":
-     sendToHosts("manual")
-
-   elif cmd[0] == "vol":
-     if len(cmd) == 2:
-       vol(cmd[1])
-     else:
-       print "Error: no volume specified"
-   elif cmd[0] == "threads":
-     if len(cmd) == 2:
-       threads(cmd[1])
-     else:
-       print "Error: no thread count  specified"
-   elif cmd[0] == "play":
-     if len(cmd) == 2:
-       play(cmd[1])
-     else:
-       print "Error: no filename specified"
-   elif cmd[0] == "phrase":
-     if len(cmd) >= 2:
-       phrase(cmd)
-     else:
-       print "Error: no filename specified"
-   else:
-     if len(cmd) == 2 and cmd[1] != "":
-       print "Error:",inp
-       print
-     printCmds()
+    inp=raw_input("schlub-> ")
+    cmd = inp.split(" ");
+    if cmd[0] in cmds.keys():
+      ret = cmds[cmd[0]](cmd)
+      if ret == -1:
+        run=False
+    else:
+      print "bad cmd:",inp
+      printCmds()
 
 
-main()
